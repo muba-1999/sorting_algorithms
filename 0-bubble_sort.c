344a7f58aa69872efb2ce699052264d8b63bcc17
muba-1999 <mukobam4@gmail.com>
2021-10-26 03:14:57 -0700

    added the print_array function

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,15 +7,21 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	int i, j, tmp;
+	size_t i, j;
+	int tmp;
 
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
 		{
-			tmp = array[j];
-			array[j] = array[j + 1];
-			array[j + 1] = tmp;
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				print_array(array, size);
+			}
+			//print_array(array, size);
 		}
 	}
 }
